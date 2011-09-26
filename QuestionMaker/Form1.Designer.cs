055beaecae62347481010cc373bafbeef05ff800
Ashiq A <ashes999@yahoo.com>
2011-09-26 17:39:39 -0400

    UI for metadata (and Dictionary in code)

--- QuestionMaker/Form1.Designer.cs
+++ QuestionMaker/Form1.Designer.cs
@@ -43,10 +43,17 @@
 			this.uxSave = new System.Windows.Forms.Button();
 			this.uxMainMenu = new System.Windows.Forms.MenuStrip();
 			this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+			this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
 			this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-			this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.label5 = new System.Windows.Forms.Label();
+			this.listView1 = new System.Windows.Forms.ListView();
+			this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.uxAddMetaData = new System.Windows.Forms.Button();
+			this.uxDeleteMetaData = new System.Windows.Forms.Button();
+			this.uxEditMetaData = new System.Windows.Forms.Button();
 			this.uxMainMenu.SuspendLayout();
 			this.SuspendLayout();
 			// 
@@ -112,7 +119,7 @@
 			this.uxCurrentQuestion.Location = new System.Drawing.Point(278, 92);
 			this.uxCurrentQuestion.Multiline = true;
 			this.uxCurrentQuestion.Name = "uxCurrentQuestion";
-			this.uxCurrentQuestion.Size = new System.Drawing.Size(494, 298);
+			this.uxCurrentQuestion.Size = new System.Drawing.Size(494, 128);
 			this.uxCurrentQuestion.TabIndex = 6;
 			// 
 			// label3
@@ -136,16 +143,16 @@
 			// 
 			// uxAnswers
 			// 
-			this.uxAnswers.Location = new System.Drawing.Point(278, 409);
+			this.uxAnswers.Location = new System.Drawing.Point(278, 249);
 			this.uxAnswers.Multiline = true;
 			this.uxAnswers.Name = "uxAnswers";
-			this.uxAnswers.Size = new System.Drawing.Size(494, 139);
+			this.uxAnswers.Size = new System.Drawing.Size(494, 128);
 			this.uxAnswers.TabIndex = 11;
 			// 
 			// label4
 			// 
 			this.label4.AutoSize = true;
-			this.label4.Location = new System.Drawing.Point(278, 393);
+			this.label4.Location = new System.Drawing.Point(278, 233);
 			this.label4.Name = "label4";
 			this.label4.Size = new System.Drawing.Size(240, 13);
 			this.label4.TabIndex = 12;
@@ -192,36 +199,99 @@
 			this.toolStripMenuItem1.Size = new System.Drawing.Size(37, 20);
 			this.toolStripMenuItem1.Text = "&File";
 			// 
+			// openToolStripMenuItem
+			// 
+			this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+			this.openToolStripMenuItem.Size = new System.Drawing.Size(115, 22);
+			this.openToolStripMenuItem.Text = "&Open ...";
+			this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+			// 
 			// saveToolStripMenuItem
 			// 
 			this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-			this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.saveToolStripMenuItem.Size = new System.Drawing.Size(115, 22);
 			this.saveToolStripMenuItem.Text = "&Save ...";
 			this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
 			// 
 			// toolStripMenuItem2
 			// 
 			this.toolStripMenuItem2.Name = "toolStripMenuItem2";
-			this.toolStripMenuItem2.Size = new System.Drawing.Size(149, 6);
+			this.toolStripMenuItem2.Size = new System.Drawing.Size(112, 6);
 			// 
 			// exitToolStripMenuItem
 			// 
 			this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-			this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.exitToolStripMenuItem.Size = new System.Drawing.Size(115, 22);
 			this.exitToolStripMenuItem.Text = "E&xit";
 			// 
-			// openToolStripMenuItem
+			// label5
 			// 
-			this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-			this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-			this.openToolStripMenuItem.Text = "&Open ...";
-			this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(278, 390);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(168, 13);
+			this.label5.TabIndex = 16;
+			this.label5.Text = "Additional per-question information";
+			// 
+			// listView1
+			// 
+			this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+			this.listView1.Location = new System.Drawing.Point(281, 406);
+			this.listView1.MultiSelect = false;
+			this.listView1.Name = "listView1";
+			this.listView1.Size = new System.Drawing.Size(491, 98);
+			this.listView1.TabIndex = 17;
+			this.listView1.UseCompatibleStateImageBehavior = false;
+			this.listView1.View = System.Windows.Forms.View.Details;
+			// 
+			// columnHeader1
+			// 
+			this.columnHeader1.Text = "Type";
+			// 
+			// columnHeader2
+			// 
+			this.columnHeader2.Text = "Information";
+			this.columnHeader2.Width = 426;
+			// 
+			// uxAddMetaData
+			// 
+			this.uxAddMetaData.Location = new System.Drawing.Point(508, 510);
+			this.uxAddMetaData.Name = "uxAddMetaData";
+			this.uxAddMetaData.Size = new System.Drawing.Size(85, 23);
+			this.uxAddMetaData.TabIndex = 18;
+			this.uxAddMetaData.Text = "Add Info";
+			this.uxAddMetaData.UseVisualStyleBackColor = true;
+			// 
+			// uxDeleteMetaData
+			// 
+			this.uxDeleteMetaData.Location = new System.Drawing.Point(687, 510);
+			this.uxDeleteMetaData.Name = "uxDeleteMetaData";
+			this.uxDeleteMetaData.Size = new System.Drawing.Size(85, 23);
+			this.uxDeleteMetaData.TabIndex = 19;
+			this.uxDeleteMetaData.Text = "Delete Info";
+			this.uxDeleteMetaData.UseVisualStyleBackColor = true;
+			// 
+			// uxEditMetaData
+			// 
+			this.uxEditMetaData.Location = new System.Drawing.Point(599, 510);
+			this.uxEditMetaData.Name = "uxEditMetaData";
+			this.uxEditMetaData.Size = new System.Drawing.Size(85, 23);
+			this.uxEditMetaData.TabIndex = 20;
+			this.uxEditMetaData.Text = "Edit Info";
+			this.uxEditMetaData.UseVisualStyleBackColor = true;
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(784, 583);
+			this.Controls.Add(this.uxEditMetaData);
+			this.Controls.Add(this.uxDeleteMetaData);
+			this.Controls.Add(this.uxAddMetaData);
+			this.Controls.Add(this.listView1);
+			this.Controls.Add(this.label5);
 			this.Controls.Add(this.uxSaveAsNew);
 			this.Controls.Add(this.uxSave);
 			this.Controls.Add(this.label4);
@@ -268,6 +338,13 @@
 		private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
 		private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
 		private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.ListView listView1;
+		private System.Windows.Forms.ColumnHeader columnHeader1;
+		private System.Windows.Forms.ColumnHeader columnHeader2;
+		private System.Windows.Forms.Button uxAddMetaData;
+		private System.Windows.Forms.Button uxDeleteMetaData;
+		private System.Windows.Forms.Button uxEditMetaData;
     }
 }
 
