3cc4f84ae20d6b44c9dcc70d08083477a93bc435
Ashiq Alibhai <ashes999@yahoo.com>
2011-10-01 17:26:18 -0400

    Added delegate/event design. Sheesh, what a bunch of extra work, eh? GO C#~! - Created generic "Action" class similar to C#'s equivalent - Added event listeners in Screen, plus the ability to notify on fade in/out - Splash screen consumes these events and works! :)

---
+++ Android/game-core/src/com/deengames/radiantwrench/utils/Action.java
@@ -0,0 +1,11 @@
+package com.deengames.radiantwrench.utils;
+
+/**
+ * Just like our C# equivalent, a generic method delegate that takes
+ * no parameters and has no return type.
+ * @author ashes999
+ *
+ */
+public interface Action {
+	void invoke();
+}
